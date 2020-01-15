cff5e22b230ba09e879a651065c2b07657513c39
AndreasErslev <57252859+AndreasErslev@users.noreply.github.com>
2020-01-15 09:33:28 +0100

    Updatering

--- carControl.cpp
+++ carControl.cpp
@@ -26,50 +26,43 @@ char carControl(char status, Motor* motor, Led* led) {
 		
 		//Bil begynder at køre
 		case 0: 
-			//PORTB = 0b00000001;
 			
 			playTrack(START_LYD);
 			motor->setDirection(1);
 			motor->setSpeed(180);
-			led->frontLight(80);
+			led->frontLight(100);
 			led->backLight(30);
 			break;
 			
 		case 1: //reflexbrik 1
-			//PORTB = 0b00000010;
 			
 			playTrack(REFLEKS_LYD1);
 			break;
 			
 		case 2: //reflexbrik 2
-			//PORTB = 0b00000100;
 			
 			playTrack(REFLEKS_LYD2);
 			//motor->setSpeed(180);
 			break;
 		
 		case 3: //reflexbrik 3
-			//PORTB = 0b00001000;
 			
 			playTrack(REFLEKS_LYD1);
 			break;
 		
 		case 4: //reflexbrik 4
-			//PORTB = 0b00010000;
 			
 			playTrack(REFLEKS_LYD2);
 			//motor->setSpeed(255);
 			break;
 			
 		case 5: //reflexbrik 5 - Siger lyd
-			//PORTB = 0b00100000;
 			
 			playTrack(REFLEKS_LYD1);
 			//motor->setSpeed(150);
 			break;
 			
 		case 6: //reflexbrik 6 - Stop / bak
-			//PORTB = 0b01000000;
 	
 			playTrack(REFLEKS_LYD2);
 			//Vend motor
@@ -87,13 +80,11 @@ char carControl(char status, Motor* motor, Led* led) {
 			break;
 			
 		case 7: //reflexbrik 6 igen - Lyd
-			//PORTB = 0b10000000;
 			
 			playTrack(REFLEKS_LYD1);
 			break;
 			
 		case 8: //reflexbrik 5 igen - Køre frem igen
-			//PORTB = 0b10000001;
 			
 			playTrack(REFLEKS_LYD2);
 			motor->setSpeed(0);
@@ -107,20 +98,16 @@ char carControl(char status, Motor* motor, Led* led) {
 			
 		case 9: //reflexbrik 5 igen - Køre frem igen
 				
-			//PORTB = 0b10000011;
 			motor->setSpeed(255);
 			playTrack(REFLEKS_LYD1);
 			break;
 		
 		case 10: //reflexbrik 6 igen - siger lyd
-			
-			//PORTB = 0b10000111;
-			
+						
 			playTrack(REFLEKS_LYD2);
 			break;
 		
 		case 11: //reflexbrik 7 - Bil stopper
-			//PORTB = 0b10001111;
 			
 			playTrack(SLUT_LYD);
 			motor->setSpeed(0);
